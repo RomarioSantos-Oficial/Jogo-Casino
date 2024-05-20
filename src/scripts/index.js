8217741a938109506dea581eb0dbd422f36f1abb
RomarioDevs <romariodevs@gmail.com>
2024-05-20 14:26:57 -0300

    tela de logi

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+// function showLoginForm() {
+//     // Esconde o elemento checkAges
+//     document.querySelector('.checkAges').style.display = 'none';
+//     // Mostra o elemento de login
+//     document.querySelector('.login').style.display = 'block';
+// }
+
+
+function handleNoClick() {
+    // Adicione qualquer ação que você deseja executar quando o botão "No" for clicado
+    window.location.href = "./src/pages/termos.html";
+}
