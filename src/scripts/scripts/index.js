2efc6c21f33e244ce2ce20de879d6fff4a688f8a
RomarioSantos-Oficial <limaromario40@gmail.com>
2024-05-20 19:48:37 -0300

    00001

--- src/scripts/scripts/index.js
+++ src/scripts/scripts/index.js
@@ -56,7 +56,7 @@ function logar() {
         // Verifica se a senha corresponde à senha salva
         if (localStorage.getItem("password") === password) {
             // Senha correta, redireciona para outra página
-            window.location.href = "./src/pages/home.html";
+            window.location.href = "./src/pages/pages/home.html";
         } else {
             // Senha incorreta
             alert("Senha incorreta.");
@@ -69,5 +69,5 @@ function logar() {
 
 function handleNoClick() {
     // Adicione qualquer ação que você deseja executar quando o botão "No" for clicado
-    window.location.href = "./src/pages/termos.html";
+    window.location.href = "./src/pages/pages/termos.html";
 }
